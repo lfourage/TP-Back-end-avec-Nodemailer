63191a5b3a30e9201635a60a38af409f6be2632b
lfourage <ludogriph@gmail.com>
2025-09-03 16:42:52 +0200

    perfection

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,12 @@ dotenv.config();
 
 const app = express();
 const PORT = 3000;
+
 app.use(cors({
   origin:'http://localhost:5173', 
   credentials: true,            
 }))
+
 app.use(express.json());
 app.use('/', router);
 
--- backend/user/route.js
+++ backend/user/route.js
@@ -1,9 +1,14 @@
 import UserController from "./controller.js";
 import express from "express";
+import path from "path";
 
 const router = express.Router();
 const userController = new UserController();
 
+router.get("/", (req, res) => {
+  const filePath = path.resolve("frontend/frontend/index.html");
+  res.sendFile(filePath);
+});
 router.post("/register", userController.createUser);
 router.get("/verify", userController.verifyEmail);
 router.post("/login", userController.loginUser);
